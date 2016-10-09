ea41a50c776a02d82ba3b8f47c460b64e2f038fd
Daniel Z Franco <zurielovsko@gmail.com>
2016-10-08 20:07:33 -0500

    Update urls.py

--- report_builder/urls.py
+++ report_builder/urls.py
@@ -21,8 +21,8 @@ urlpatterns = [
     url(r'^export_to_report/$', views.ExportToReport.as_view(), name="export_to_report"),
     url(r'^api/', include(router.urls)),
     url(r'^api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/related_fields', staff_member_required(api_views.RelatedFieldsView.as_view()), name="related_fields"),
-    url(r'^api/fields', staff_member_required(api_views.FieldsView.as_view()), name="fields"),
+    url(r'^api/related_fields', api_views.RelatedFieldsView.as_view(), name="related_fields"),
+    url(r'^api/fields', api_views.FieldsView.as_view(), name="fields"),
     url(r'^api/report/(?P<report_id>\w+)/generate/', staff_member_required(api_views.GenerateReport.as_view()), name="generate_report"),
     url('^report/(?P<pk>\d+)/$', views.ReportSPAView.as_view(), name="report_update_view"),
 ]
